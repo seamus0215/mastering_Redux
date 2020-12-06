0e939d74de4e58009ba8e87b978f8adf641f1bbc
Seamus <seamusugochukwu@gmail.com>
2020-12-06 05:41:15 +0100

    created manage course functionality which included creating and saving a course and handle redirects

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -1,20 +1,47 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import * as courseActions from "../../redux/actions/courseActions";
 import PropTypes from "prop-types";
 import { bindActionCreators } from "redux";
 import CourseList from "./CourseList";
+import * as courseActions from "../../redux/actions/courseActions";
+import * as authorActions from "../../redux/actions/authorActions";
+import { Redirect } from "react-router-dom";
 
 class CoursesPage extends Component {
+  state = {
+    redirectToAddCoursePage: false,
+  };
+
   componentDidMount() {
-    this.props.actions.loadCourses().catch((error) => {
-      alert("Loading courses failed" + error);
-    });
+    const { courses, authors, actions } = this.props;
+
+    if (courses.length === 0) {
+      actions.loadCourses().catch((error) => {
+        alert("Loading courses failed" + error);
+      });
+    }
+
+    if (authors.length === 0) {
+      actions.loadAuthors().catch((error) => {
+        alert("Loading authors failed" + error);
+      });
+    }
   }
+
   render() {
     return (
       <>
-        <h2>Courses</h2>
+        {this.redirectToAddCoursePage && <Redirect to="/course" />}
+        <div className="course_page_header">
+          <h2>Courses</h2>
+          <button
+            style={{ marginBottom: 20 }}
+            className="btn btn-primary add-course"
+            onClick={() => this.setState({ redirectToAddCoursePage: true })}
+          >
+            Add Course
+          </button>
+        </div>
         <CourseList courses={this.props.courses} />
       </>
     );
@@ -22,20 +49,33 @@ class CoursesPage extends Component {
 }
 
 CoursesPage.propTypes = {
+  authors: PropTypes.array.isRequired,
   courses: PropTypes.array.isRequired,
   actions: PropTypes.object.isRequired,
 };
 
 function mapStateToProps(state) {
+  // It's important to know that you can destructure the state and eliminate the (state.courses) in the return.
   return {
-    // It's important to know that you can destructure the state and eliminate the (state.courses) in the return.
-    courses: state.courses,
+    // It's good practise to check that both courses and authors data is available before we map.
+    courses: state.authors.length === 0 ? [] : state.courses.map((course) => {
+      return {
+        ...course,
+        authorName: state.authors.find(
+          (author) => author.id === course.authorId
+        ).name,
+      };
+    }),
+    authors: state.authors,
   };
 }
 
 function mapDispatchToProps(dispatch) {
   return {
-    actions: bindActionCreators(courseActions, dispatch),
+    actions: {
+      loadCourses: bindActionCreators(courseActions.loadCourses, dispatch),
+      loadAuthors: bindActionCreators(authorActions.loadAuthors, dispatch),
+    },
   };
 }
 
---
+++ src/components/courses/ManageCoursePage.js
@@ -0,0 +1,81 @@
+import React, { useState, useEffect } from "react";
+import { connect } from "react-redux";
+import { loadCourses, saveCourse } from "../../redux/actions/courseActions";
+import { loadAuthors } from "../../redux/actions/authorActions";
+import PropTypes from "prop-types";
+import CourseForm from "./CourseForm";
+import { newCourse } from "../../../tools/mockData";
+
+function ManageCoursePage({
+  courses,
+  authors,
+  loadAuthors,
+  loadCourses,
+  ...props
+}) {
+  const [course, setCourse] = useState({ ...props.course });
+  const [errors, setErrors] = useState({});
+  useEffect(() => {
+    if (courses.length === 0) {
+      loadCourses().catch((error) => {
+        alert("Loading courses failed" + error);
+      });
+    }
+    if (authors.length === 0) {
+      loadAuthors().catch((error) => {
+        alert("Loading authors failed" + error);
+      });
+    }
+  }, []);
+
+  function handleChange(e) {
+    const { name, value } = e.target;
+    setCourse((prevCourse) => ({
+      ...prevCourse,
+      [name]: name === "authorId" ? parseInt(value, 10) : value,
+    }));
+  }
+
+  function handleSave(e) {
+    e.preventDefault();
+    saveCourse(course);
+    alert("course detail" + course.title + course.authorId + course.category);
+  }
+
+  return (
+    <CourseForm
+      course={course}
+      errors={errors}
+      authors={authors}
+      onChange={handleChange}
+      onSave={handleSave}
+    />
+  );
+}
+
+ManageCoursePage.propTypes = {
+  course: PropTypes.object.isRequired,
+  authors: PropTypes.array.isRequired,
+  courses: PropTypes.array.isRequired,
+  loadCourses: PropTypes.func.isRequired,
+  loadAuthors: PropTypes.func.isRequired,
+  saveCourse: PropTypes.func.isRequired,
+};
+
+function mapStateToProps(state) {
+  return {
+    course: newCourse,
+    courses: state.courses,
+    authors: state.authors,
+  };
+}
+
+const mapDispatchToProps = {
+  // AN EASIER BUT POTENTIALLY CONFUSING WAY TO DECLARE MapDispatchToProps()
+  // If we declare mapDispatchToProps as an object instead of a function, each property will automatically be bound to dispatch.
+  loadCourses,
+  loadAuthors,
+  saveCourse,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(ManageCoursePage);
